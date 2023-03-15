438890896903db69687c82f980d5cc7a879bb7c4
DavidTrujillo123 <davot098@gmail.com>
2023-03-15 12:56:46 -0500

    Implementacion del carrito de compras y arreglo de bugs en los menus

--- main.js
+++ main.js
@@ -1,13 +1,28 @@
 const menuEmail = document.querySelector('.navbar-email');
 const desktopMenu = document.querySelector('.desktop-menu');
-const menuHamIcon = document.querySelector('.menu');
+const hamburgerIcon = document.querySelector('.menu');
 const mobileMenu = document.querySelector('.mobile-menu');
+const menuCardIcon = document.querySelector('.navbar-shopping-cart');
+const aside = document.querySelector('.product-detail');
 
 function toggleDesktopMenu(){
     desktopMenu.classList.toggle('inactive');
 }
 function toggleMovileMenu(){
+    let isCardAsideOpen = !aside.classList.contains('inactive');
+    if(isCardAsideOpen){
+        aside.classList.add('inactive');
+    }
     mobileMenu.classList.toggle('inactive');
 }
+function toggleCardAside(){
+    let isMobileMenuOpen = !mobileMenu.classList.contains('inactive');
+    if(isMobileMenuOpen){
+        mobileMenu.classList.add('inactive');
+    }
+    aside.classList.toggle('inactive');
+}
+
 menuEmail.addEventListener('click', toggleDesktopMenu);
-menuHamIcon.addEventListener('click', toggleMovileMenu);
+hamburgerIcon.addEventListener('click', toggleMovileMenu);
+menuCardIcon.addEventListener('click', toggleCardAside);
